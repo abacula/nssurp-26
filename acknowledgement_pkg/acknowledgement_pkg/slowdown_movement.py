915f16753a8aed8aa5aded7c7e1dfaeedc3e054a
nhat0604 <nhat.nguyen@plu.edu>
2026-06-25 13:29:27 -0700

    Added light changing when a person is detected

--- acknowledgement_pkg/acknowledgement_pkg/slowdown_movement.py
+++ acknowledgement_pkg/acknowledgement_pkg/slowdown_movement.py
@@ -11,12 +11,12 @@ class SlowdownMovement(Node):
     def __init__(self):
         super().__init__('slowdown_node')
 
-        self.SOUND = True               # do we want sounds
-        self.LIGHTS = False        # do we want lights (not implemented yet)
-
+        self.SOUND = False               # do we want sounds
+        self.LIGHTS = True        # do we want lights (not implemented yet)
+        self.FADE_RATE = 5
         self.FORWARD_SPD = 0.5          # m/s
         self.CONF_THRESH = 0.75         # min confidence
-        self.TRIGGER_HEIGHT = 60        # bbox_height that starts the slowdown
+        self.TRIGGER_HEIGHT = 70        # bbox_height that starts the slowdown
 
         self.publisher = self.create_publisher(Twist, '/robot4/cmd_vel_unstamped', 10)
         self.ack_sub = self.create_subscription(HallwayAck, '/robot4/hallway_ack', self.hallway_cb, 10)
@@ -55,21 +55,22 @@ class SlowdownMovement(Node):
         else:
             audio_msg = AudioNoteVector()
             audio_msg.append = False
-            self.get_logger().info("Sound Killed.")
-
+           
         if self.LIGHTS:
             light_msg = String()
-            if twist.linear.x < self.FORWARD_SPD:
-                light_msg.data = "something"
-            else:
-                light_msg.data = "something else"
+            if twist.linear.x == 0:
+                light_msg.data = "instant 0"
+            elif twist.linear.x < self.FORWARD_SPD:
+                light_msg.data =  f'fade {self.FADE_RATE} 42'
 
+            else:
+                light_msg.data = "instant 85"
         self.light_pub.publish(light_msg)    
         self.sound_pub.publish(audio_msg)
         self.publisher.publish(twist)
 
     def get_speed(self, box_height, max_speed = 0.5, min_speed=0.05):
-        ratio = self.TRIGGER_HEIGHT / box_height                  # 0.0 (close) to 1.0 (far)
+        ratio = (self.TRIGGER_HEIGHT) / (box_height*1.35)                   # 0.0 (close) to 1.0 (far)
         return float(min_speed + ratio * (max_speed - min_speed))
 
 def main(args=None):
